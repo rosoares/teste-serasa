bfdf29b0cbda71cee2185128f883837cd9af9f8c
rosoares8899 <rodrigosoares8899@gmail.com>
2022-05-26 00:46:56 -0300

    Teste que não foi no último commit

--- src/test/java/com/example/serasa/service/PessoaServiceTest.java
+++ src/test/java/com/example/serasa/service/PessoaServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.serasa.service;
 
+import com.example.serasa.dto.afinidade.AfinidadeDTO;
 import com.example.serasa.dto.pessoa.NovaPessoaDTO;
+import com.example.serasa.dto.pessoa.PessoaDTO;
+import com.example.serasa.dto.score.ScoreDTO;
 import com.example.serasa.model.Pessoa;
 import com.example.serasa.repository.PessoaRepository;
 import com.example.serasa.service.impl.PessoaServiceImpl;
@@ -14,6 +17,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -25,6 +31,12 @@ public class PessoaServiceTest {
 
     @Mock
     private PessoaRepository repository;
+
+    @Mock
+    private ScoreService scoreService;
+
+    @Mock
+    private AfinidadeService afinidadeService;
 
     @Mock
     private ModelMapper mapper;
@@ -51,6 +63,87 @@ public class PessoaServiceTest {
         assertEquals(requisicao.getRegiao(), pessoaArgumentCaptor.getValue().getRegiao());
         assertNotNull(pessoaArgumentCaptor.getValue().getId());
         assertNotNull(pessoaArgumentCaptor.getValue().getDataInclusao());
+    }
+
+    @Test
+    void testeEncontraPessoaComSucesso() {
+
+        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.of(respostaPessoaValida()));
+        Mockito.when(scoreService.encontraFaixaPorScore(Mockito.anyInt())).thenReturn(scoreInsuficiente());
+        Mockito.when(afinidadeService.encontraPelaRegiao(Mockito.anyString())).thenReturn(afinidadeDTO());
+        Mockito.when(mapper.map(Mockito.any(Pessoa.class), Mockito.any())).thenReturn(pessoaDTO1());
+
+        PessoaDTO resposta = pessoaService.encontrar(Mockito.anyLong());
+
+        assertEquals(resposta.getEstados().get(0), "MG");
+        assertEquals(resposta.getScoreDescricao(), "Insuficiente");
+
+    }
+
+    @Test
+    void testeEncontraTodasAsPessoasComSucesso() {
+
+        Mockito.when(repository.findAll()).thenReturn(List.of(respostaPessoaValida(), respostaPessoaComScoreAceitavel()));
+        Mockito.when(afinidadeService.encontraPelaRegiao(Mockito.anyString())).thenReturn(afinidadeDTO());
+        Mockito.when(scoreService.encontraFaixaPorScore(Mockito.eq(PessoaTestBuilder.PessoaBuilder.score))).thenReturn(scoreInsuficiente());
+        Mockito.when(scoreService.encontraFaixaPorScore(Mockito.eq(600))).thenReturn(scoreAceitavel());
+        Mockito.when(mapper.map(Mockito.any(Pessoa.class), Mockito.any())).thenReturn(pessoaDTO1(), pessoaDTO2());
+
+        List<PessoaDTO> resposta = pessoaService.encontrarTodas();
+
+        assertEquals(resposta.get(0).getScoreDescricao(), "Insuficiente");
+        assertEquals(resposta.get(1).getScoreDescricao(), "Aceitável");
+
+    }
+
+    private PessoaDTO pessoaDTO1() {
+        return PessoaDTO.builder()
+                .nome(PessoaTestBuilder.PessoaBuilder.nome)
+                .telefone(PessoaTestBuilder.PessoaBuilder.telefone)
+                .idade(PessoaTestBuilder.PessoaBuilder.idade)
+                .build();
+    }
+
+    private PessoaDTO pessoaDTO2() {
+        return PessoaDTO.builder()
+                .nome("Fulano de tal")
+                .telefone("38 99999-9998")
+                .idade(45)
+                .build();
+    }
+
+    private AfinidadeDTO afinidadeDTO() {
+
+        return AfinidadeDTO.builder()
+                .estados(List.of("MG", "RJ", "SP", "ES"))
+                .regiao("sudeste")
+                .build();
+    }
+
+    private ScoreDTO scoreInsuficiente() {
+
+        return ScoreDTO.builder()
+                .scoreDescricao("Insuficiente")
+                .scoreInicial(0)
+                .scoreFinal(200)
+                .build();
+    }
+
+    private ScoreDTO scoreAceitavel() {
+
+        return ScoreDTO.builder()
+                .scoreDescricao("Aceitável")
+                .scoreInicial(501)
+                .scoreFinal(700)
+                .build();
+    }
+
+    private Pessoa respostaPessoaComScoreAceitavel() {
+
+        Pessoa pessoa = respostaPessoaValida();
+        pessoa.setScore(600);
+
+        return pessoa;
     }
 
     private Pessoa respostaPessoaValida() {
